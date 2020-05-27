ad961ba266f6e007c4b484d78c1f1d37d60f408f
Stephen <stephen.adsl@gmail.com>
2020-05-27 12:18:43 -0700

    Updates

---
+++ precache-manifest.4fc34c66be58c04accd3fcd4533e8aba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba6961ae2c9dc064942b2f567979fa8e",
+    "url": "/sql-gen/index.html"
+  },
+  {
+    "revision": "f3cd68d58b16c6a77cb3",
+    "url": "/sql-gen/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c677aaa28ba4bdc13170",
+    "url": "/sql-gen/static/js/2.aee4d38e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sql-gen/static/js/2.aee4d38e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3cd68d58b16c6a77cb3",
+    "url": "/sql-gen/static/js/main.4b86e572.chunk.js"
+  },
+  {
+    "revision": "083843877ae03fd4855d",
+    "url": "/sql-gen/static/js/runtime-main.2cf6057f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sql-gen/static/media/logo.5d5d9eef.svg"
+  }
+]);
