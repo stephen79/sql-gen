9f372a061c9a6b7ceab7e90d87e13b7aa8c36e2a
Stephen <stephen.adsl@gmail.com>
2020-05-27 12:09:28 -0700

    Updates

---
+++ precache-manifest.64cd8bdeedcdc5306cafbc1a9153881c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40275b2e10854d8562111fc89813a322",
+    "url": "/index.html"
+  },
+  {
+    "revision": "406e1ee83b70352bb0cf",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c677aaa28ba4bdc13170",
+    "url": "/static/js/2.aee4d38e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.aee4d38e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "406e1ee83b70352bb0cf",
+    "url": "/static/js/main.9706fe80.chunk.js"
+  },
+  {
+    "revision": "28a960ace5f41b3e8ff0",
+    "url": "/static/js/runtime-main.5bf72551.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
